d79769a0c321ba9fecc422a8dfe78156ed8ccc97
diandabukke2808@gmail.com <diandabukke2808@gmail.com>
2024-10-24 19:58:21 +0700

    update

--- assets/part/redirect.js
+++ assets/part/redirect.js
@@ -3,23 +3,22 @@
 
     // Memeriksa apakah URL mengandung domain dialogika.co
     if (/^(https?:\/\/)?(www\.)?dialogika\.co/.test(currentURL)) {
-        // Menggunakan Fetch untuk mengirim GET request ke URL
-        fetch(currentURL, { method: 'GET' })
-            .then(response => {
-                console.log('Status code:', response.status); // Logging status response untuk pengecekan
-                if (response.status === 404) {
+        // Membuat XMLHttpRequest untuk mengirim GET request
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', currentURL, true);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                console.log('Status code:', xhr.status); // Logging status response untuk pengecekan
+                if (xhr.status === 404) {
                     // Jika status 404, arahkan ke halaman 404
                     window.location.href = 'https://www.dialogika.co/404.html';
-                } else if (response.status >= 200 && response.status < 300) {
+                } else if (xhr.status >= 200 && xhr.status < 300) {
                     // Jika status 2xx (sukses), tidak perlu melakukan apa-apa
                     console.log("Halaman ditemukan, status OK");
                 }
-            })
-            .catch(error => {
-                // Jika terjadi kesalahan lain (seperti jaringan), arahkan ke halaman 404
-                console.error("Error occurred: ", error);
-                window.location.href = 'https://www.dialogika.co/404.html';
-            });
+            }
+        };
+        xhr.send();  // Mengirim request
     } else {
         // Jika bukan domain yang valid, arahkan ke halaman 404
         window.location.href = 'https://www.dialogika.co/404.html';
