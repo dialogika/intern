263498e2e7dabaf5d5bfcb581b36576d7caea7c8
diandabukke2808@gmail.com <diandabukke2808@gmail.com>
2024-10-24 20:10:03 +0700

    update

--- assets/part/redirect.js
+++ assets/part/redirect.js
@@ -3,22 +3,28 @@
 
     // Memeriksa apakah URL mengandung domain dialogika.co
     if (/^(https?:\/\/)?(www\.)?dialogika\.co/.test(currentURL)) {
-        // Membuat XMLHttpRequest untuk mengirim GET request
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', currentURL, true);
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4) {
-                console.log('Status code:', xhr.status); // Logging status response untuk pengecekan
-                if (xhr.status === 404) {
-                    // Jika status 404, arahkan ke halaman 404
-                    window.location.href = 'https://www.dialogika.co/404.html';
-                } else if (xhr.status >= 200 && xhr.status < 300) {
-                    // Jika status 2xx (sukses), tidak perlu melakukan apa-apa
-                    console.log("Halaman ditemukan, status OK");
+        // Fungsi untuk memvalidasi URL
+        function checkValidURL(url) {
+            var xmlhttp = new XMLHttpRequest();
+            xmlhttp.onreadystatechange = function() {
+                if (xmlhttp.readyState === 4) {
+                    console.log('Status code:', xmlhttp.status); // Logging status response untuk pengecekan
+                    if (xmlhttp.status === 404) {
+                        // Jika status 404, arahkan ke halaman 404
+                        window.location.href = 'https://www.dialogika.co/404.html';
+                    } else if (xmlhttp.status >= 200 && xmlhttp.status < 300) {
+                        // Jika status 2xx (sukses), tidak perlu melakukan apa-apa
+                        console.log("Halaman ditemukan, status OK");
+                    }
                 }
-            }
-        };
-        xhr.send();  // Mengirim request
+            };
+            // AJAX configuration
+            xmlhttp.open("GET", url, true); // Gunakan "true" untuk asynchronous request
+            xmlhttp.send();
+        }
+
+        // Memanggil fungsi untuk memvalidasi URL saat ini
+        checkValidURL(currentURL);
     } else {
         // Jika bukan domain yang valid, arahkan ke halaman 404
         window.location.href = 'https://www.dialogika.co/404.html';
