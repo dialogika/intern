bc6da927011016a6fc52fbbb21524a659acdd0fd
diandabukke2808@gmail.com <diandabukke2808@gmail.com>
2024-10-24 20:19:34 +0700

    update

--- assets/part/redirect.js
+++ assets/part/redirect.js
@@ -1,32 +1,26 @@
 (function() {
-    const currentURL = window.location.href;
+    const currentURL = window.location.href; // Get the current page URL
 
-    // Memeriksa apakah URL mengandung domain dialogika.co
+    // Check if the URL contains the domain dialogika.co
     if (/^(https?:\/\/)?(www\.)?dialogika\.co/.test(currentURL)) {
-        // Fungsi untuk memvalidasi URL
-        function checkValidURL(url) {
-            var xmlhttp = new XMLHttpRequest();
-            xmlhttp.onreadystatechange = function() {
-                if (xmlhttp.readyState === 4) {
-                    console.log('Status code:', xmlhttp.status); // Logging status response untuk pengecekan
-                    if (xmlhttp.status === 404) {
-                        // Jika status 404, arahkan ke halaman 404
-                        window.location.href = 'https://www.dialogika.co/404.html';
-                    } else if (xmlhttp.status >= 200 && xmlhttp.status < 300) {
-                        // Jika status 2xx (sukses), tidak perlu melakukan apa-apa
-                        console.log("Halaman ditemukan, status OK");
-                    }
+        // Create an XMLHttpRequest to send a GET request
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', currentURL, true); // Asynchronous request
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) { // Request is complete
+                console.log('Status code:', xhr.status); // Log status response for checking
+                if (xhr.status === 404) {
+                    // If status is 404, redirect to the custom 404 page
+                    window.location.href = 'https://www.dialogika.co/404.html';
+                } else if (xhr.status >= 200 && xhr.status < 300) {
+                    // If status is 2xx (success), log a success message
+                    console.log("Halaman ditemukan, status OK");
                 }
-            };
-            // AJAX configuration
-            xmlhttp.open("GET", url, true); // Gunakan "true" untuk asynchronous request
-            xmlhttp.send();
-        }
-
-        // Memanggil fungsi untuk memvalidasi URL saat ini
-        checkValidURL(currentURL);
+            }
+        };
+        xhr.send();  // Send the request
     } else {
-        // Jika bukan domain yang valid, arahkan ke halaman 404
+        // If the URL is not valid, redirect to the custom 404 page
         window.location.href = 'https://www.dialogika.co/404.html';
     }
 })();
