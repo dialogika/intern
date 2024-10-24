ba2b2fb60d3d995db93c38bfca8db63896304f69
diandabukke2808@gmail.com <diandabukke2808@gmail.com>
2024-10-24 19:23:13 +0700

    update

--- assets/part/redirect.js
+++ assets/part/redirect.js
@@ -15,9 +15,15 @@
                 // Jika terjadi kesalahan (misal jaringan), arahkan ke halaman 404
                 window.location.href = 'https://www.dialogika.co/404.html';
             });
+
+        // Fallback otomatis untuk mengarahkan ke 404 jika tidak ada response setelah waktu tertentu
+        setTimeout(() => {
+            window.location.href = 'https://www.dialogika.co/404.html';
+        }, 5000); // Redirect ke 404 setelah 5 detik jika tidak ada respons
     } else {
         // Jika bukan domain yang valid, arahkan ke halaman 404
         window.location.href = 'https://www.dialogika.co/404.html';
     }
 })();
+
 
