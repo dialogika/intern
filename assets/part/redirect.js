ddfc60a0fbb79671914e2f288a14d02be6d15ea7
diandabukke2808@gmail.com <diandabukke2808@gmail.com>
2024-10-24 19:32:06 +0700

    update

--- assets/part/redirect.js
+++ assets/part/redirect.js
@@ -5,21 +5,16 @@
     if (/^(https?:\/\/)?(www\.)?dialogika\.co/.test(currentURL)) {
         // Menggunakan Fetch untuk memeriksa keberadaan URL
         fetch(currentURL)
-            .then(response => {
-                if (!response.ok) {
-                    // Jika tidak ada (404), arahkan ke halaman 404
-                    window.location.href = 'https://www.dialogika.co/404.html';
-                }
-            })
-            .catch(() => {
-                // Jika terjadi kesalahan (misal jaringan), arahkan ke halaman 404
-                window.location.href = 'https://www.dialogika.co/404.html';
-            });
-
-        // Fallback otomatis untuk mengarahkan ke 404 jika tidak ada response setelah waktu tertentu
-        setTimeout(() => {
+    .then(response => {
+        console.log(response.status); // Cek status code
+        if (!response.ok) {
             window.location.href = 'https://www.dialogika.co/404.html';
-        }, 5000); // Redirect ke 404 setelah 5 detik jika tidak ada respons
+        }
+    })
+    .catch(() => {
+        window.location.href = 'https://www.dialogika.co/404.html';
+    });
+
     } else {
         // Jika bukan domain yang valid, arahkan ke halaman 404
         window.location.href = 'https://www.dialogika.co/404.html';
