5954e22e4072875efee80ab15bb54cf3a75b0bb5
Frengky04 <221712051@students.uajy.ac.id>
2025-10-23 14:37:26 +0700

    header baru

--- assets/js/headerfooter.js
+++ assets/js/headerfooter.js
@@ -49,6 +49,7 @@ class Header extends HTMLElement {
                   <ul>
                     <li><a href="https://www.dialogika.co/program/first-class.html">First Class <span class="badge rounded-pill text-bg-success">Best Buy</span></a></li>
                     <li><a href="https://www.dialogika.co/program/first-kids.html">First Kids</a></li>
+                    <li><a href="https://www.dialogika.co/program/first-lite.html">First Lite</a></li>
                     <li><hr/></li>
                     <li><a href="https://www.dialogika.co/program/first-private.html">First Private</a></li>
                   </ul>
